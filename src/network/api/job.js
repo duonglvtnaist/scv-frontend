8451396a5fe75b2374e0d10d9ce14cf62cab2172
zunvu <vudung.siaedu@gmail.com>
2022-04-07 23:11:56 +0700

    fix create job should work

--- src/network/api/job.js
+++ src/network/api/job.js
@@ -6,13 +6,6 @@ export const createJob = async (job) => {
 }
 
 export const updateJob = async (job) => {
-
   return await axiosClient.put('/job', job);
-
-}
-
-export const searchSimpleJob = async (query) => {
-
-  return await axiosClient.get('/simple-search', { params: query });
 
 }
