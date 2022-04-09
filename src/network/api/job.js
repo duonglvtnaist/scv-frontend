062c47c05b101b22d405e08f6556b1eafd1f3674
willup <nhan.dv8289@gmail.com>
2022-04-09 23:08:09 +0700

    add search job

--- src/network/api/job.js
+++ src/network/api/job.js
@@ -1,4 +1,4 @@
-import { axiosClient } from '../apiClient';
+import axiosClient  from '../apiClient';
 
 export const createJob = async (job) => {
   return await axiosClient.post('/job', job);
@@ -7,5 +7,14 @@ export const createJob = async (job) => {
 
 export const updateJob = async (job) => {
   return await axiosClient.put('/job', job);
-
 }
+
+export const searchSmart = async (objSearch) => {
+  //search job
+  console.log('service==========',objSearch)
+  return axiosClient.get('/advance-search',{
+    params:{
+      ...objSearch
+    }
+  })
+};
